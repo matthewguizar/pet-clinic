a160f2fd1334d792c4842118c25883f5992d5e00
Matthew Guizar <matthewrguizar@gmail.com>
2022-12-07 12:54:43 -0800

    converted Owners to JPA entities. closes #35

--- petclinicdata/src/main/java/com/springframework/model/Person.java
+++ petclinicdata/src/main/java/com/springframework/model/Person.java
@@ -1,8 +1,14 @@
 package com.springframework.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity{
-     private String firstName;
-     private String lastName;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
 
     public String getFirstName() {
         return firstName;
