cba3a3f787983d693c809b85a7d7f6c2633bd82b
Matthew Guizar <matthewrguizar@gmail.com>
2022-12-07 17:04:22 -0800

    created visit map service. closes #44

---
+++ petclinicdata/src/main/java/com/springframework/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.springframework.services;
+
+import com.springframework.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ petclinicdata/src/main/java/com/springframework/services/map/VisitMapService.java
@@ -0,0 +1,37 @@
+package com.springframework.services.map;
+
+import com.springframework.model.Visit;
+import com.springframework.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null ||
+        visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("invalid");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
