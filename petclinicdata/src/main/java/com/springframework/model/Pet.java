3f1349ef238e29dc426295eefbd69e3f1a649309
Matthew Guizar <matthewrguizar@gmail.com>
2022-12-15 16:50:34 -0800

    display owner record. closes #51

--- petclinicdata/src/main/java/com/springframework/model/Pet.java
+++ petclinicdata/src/main/java/com/springframework/model/Pet.java
@@ -30,4 +30,5 @@ public class Pet extends  BaseEntity{
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
 
+
 }
--- petclinicweb/src/main/java/com/springframework/controllers/OwnerController.java
+++ petclinicweb/src/main/java/com/springframework/controllers/OwnerController.java
@@ -4,7 +4,10 @@ package com.springframework.controllers;
 import com.springframework.services.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @RequestMapping("/owners")
@@ -30,5 +33,12 @@ public class OwnerController {
         return "notImplemented";
     }
 
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId){
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
+    }
 
 }
