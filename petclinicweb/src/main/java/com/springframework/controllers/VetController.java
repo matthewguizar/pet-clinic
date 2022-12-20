627a13a0b43edf864da7c8d466cf68295ecb552b
Matthew Guizar <matthewrguizar@gmail.com>
2022-12-19 17:26:45 -0800

    implemented vets json endpoint

--- petclinicweb/src/main/java/com/springframework/controllers/VetController.java
+++ petclinicweb/src/main/java/com/springframework/controllers/VetController.java
@@ -1,9 +1,15 @@
 package com.springframework.controllers;
 
+import com.springframework.model.Vet;
 import com.springframework.services.*;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -20,5 +26,10 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 }
